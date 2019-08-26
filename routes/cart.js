d65f3bacc7621bd2c0c5322f856c43d050846fab
Sabrina Brooks <51006280+SabsBrooks@users.noreply.github.com>
2019-08-26 08:44:07 -0400

    Add files via upload

---
+++ routes/cart.js
@@ -0,0 +1,28 @@
+const cart = [
+  {
+    product: "Drums",
+    id: "1az3sx",
+    price: 2500,
+    quantity: 2
+  },
+  {
+    product: "Bass",
+    id: "2dc5fv",
+    price: 850,
+    quantity: 1
+  },
+  {
+    product: "Guitar",
+    id: "6gb8hn",
+    price: 700,
+    quantity: 3
+  },
+  {
+    product: "Electric Piano",
+    id: "4jm9kl",
+    price: 900,
+    quantity: 2
+  }
+];
+
+module.exports = cart;
