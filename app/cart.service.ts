d840d48b421e38ed000d343013b8ab5e7afbf1af
Sabrina Brooks <51006280+SabsBrooks@users.noreply.github.com>
2019-08-29 16:06:29 -0400

    Add files via upload

--- app/cart.service.ts
+++ app/cart.service.ts
@@ -8,7 +8,19 @@ import { HttpClient } from "@angular/common/http";
 export class CartService {
 
   getAllItems(): Observable<any> {
-    return this.http.get(`http://localhost:5000/cart-items`);
+    return this.http.get(`http://localhost:5000/shoppingcart`);
+  }
+
+  addItem(item: any): Observable<any> {
+    return this.http.post("http://localhost:5000/shoppingcart", item);
+  }
+
+  removeItem(id): Observable<any> {
+    return this.http.delete(`http://localhost:5000/shoppingcart/${id}`);
+  }
+
+  updateItem(item: any): Observable<any> {
+    return this.http.put(`http://localhost:5000/shoppingcart/${item.id}`, item);
   }
 
   constructor(private http: HttpClient) {}
